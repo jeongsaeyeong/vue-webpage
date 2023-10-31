8a1eead6336d9df18ac23619aaa31a7da7b66b6b
jeongsaeyeong <ooooo0516@naver.com>
2023-10-31 11:15:22 +0900

    20231031

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -10,14 +10,15 @@ import Text from "@/components/Text.vue"
 </script>
 
 <template>
-  <Header />
+  <Header element="nexon" />
   <main>
-    <Slider />
-    <Image />
-    <ImgText />
-    <Card />
-    <Banner />
-    <Text />
+    <Slider element="nexon" />
+    <Image element="nexon section" title="포트폴리오가 실력이다." />
+    <ImgText element="section nexon gray" title="이미지 텍스트 유형" />
+    <Card element="nexon section" title="웹스토리보이 강의" />
+    <Banner element="nexon section" title="배너 영역" />
+    <Text element="nexon section" title="스크립트를 익히는 방법" />
   </main>
-  <Footer />
+  <Footer element="next section gray" />
 </template>
+
